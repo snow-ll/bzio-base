6d1eee4502e038132136dd591c1618aa97c7125e
snow <18643063175@163.com>
2023-09-19 07:15:58 +0800

    优化feign拦截器

--- bzio-common/bzio-common-security/src/main/java/org/bzio/common/security/filter/JwtAuthenticationTokenFilter.java
+++ bzio-common/bzio-common-security/src/main/java/org/bzio/common/security/filter/JwtAuthenticationTokenFilter.java
@@ -5,11 +5,11 @@ import org.bzio.common.core.util.JwtUtil;
 import org.bzio.common.core.util.StringUtil;
 import org.bzio.common.core.config.AuthConfig;
 import org.bzio.common.redis.service.StringRedisService;
+import org.bzio.common.security.entity.LoginUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
@@ -46,10 +46,12 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader(authConfig.getHeader());
         if (StringUtil.isNotEmpty(authHeader)) {
-            String authToken = stringRedisService.get(authConfig.getPrefix() + AesUtil.decrypt(authHeader, authConfig.getAesKey()));
+            String key = AesUtil.decrypt(authHeader, authConfig.getAesKey());
+            String authToken = stringRedisService.get(authConfig.getPrefix() + key);
             String username = jwtUtil.getUsernameFromToken(authToken);
             if (StringUtil.isNotEmpty(username) && jwtUtil.validateToken(authToken, username)) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                LoginUser userDetails = (LoginUser) this.userDetailsService.loadUserByUsername(username);
+                userDetails.setKey(key);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 log.info("authenticated user:{}", username);
